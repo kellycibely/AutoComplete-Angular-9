9a45316fd20fb665e4d7420ba91c87e3715444aa
kelly <kellycibely@gmail.com>
2020-03-01 23:02:34 -0300

    created autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'autocomplet';
+  clientObject: string;
+  clientSimples: string;
+
+  options: string[] = ['Angular', 'Angular Material', 'Java', 'Vue', 'Ruby'];
+
+  objectOptions = [
+    {name: 'Angular'},
+    {name: 'Angular Material'},
+    {name: 'Java'},
+    {name: 'Vue'}
+  ];
+
+  client = new FormControl();
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit(){
+    this.filteredOptions = this.client.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+  private _filter(value: string): string[]{
+    const filterValue = value.toLowerCase()
+    return this.options.filter(option => option.toLowerCase().includes(filterValue)) 
+  }
+  
+
+  displayFn(subject){
+    return subject ? subject.name : undefined;
+  }
+
+  saveFilter(){
+    alert(this.client.value);
+  }
+
+  saveObject(){
+    alert(this.clientObject);
+  }
+
+  save(){
+    alert(this.clientSimples);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
